fdff1e31cdf5439559c428007836f00b2036cdcb
Yahia Ewida <yahia.ewida@integrant.com>
2022-02-13 18:06:41 +0200

    adding ci.sh for jenkins

---
+++ Numero/ci.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+xcodebuild -project 'Numero/Numero.xcodeproj' -scheme 'Numero' -destination 'platform=iOS Simulator,name=iPhone 11' test
+xcodebuild -project 'Numero/Numero.xcodeproj' -scheme 'Numero' -destination 'generic/platform=iOS' -configuration Release build CODE_SIGNING_ALLOWED=NO
+
